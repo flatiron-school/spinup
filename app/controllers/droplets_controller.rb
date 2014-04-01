f24eef989017a426c735c62940deeb3a94a93a68
Logan Hasson <logan.hasson@gmail.com>
2014-03-31 20:48:46 -0400

    Set up DropletsController

---
+++ app/controllers/droplets_controller.rb
@@ -0,0 +1,4 @@
+class DropletsController < ApplicationController
+  def new
+  end
+end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+    set_session_data
+
+    if session['token']
+      redirect_to new_droplets_path
+    end
   end
-  
+
+  private
+    def set_session_data
+      github_data = request.env['omniauth.auth']
+      session['uid'] = github_data.uid
+      session['email'] = github_data.info.email
+      session['token'] = github_data.credentials.token
+    end
+
 end
